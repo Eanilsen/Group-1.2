db345f160db5d83c57dc570db0b6282792439334
Jeyloh <jlh.94@hotmail.com>
2015-10-20 00:09:20 +0200

    new master project LES > SLIT, added configuration

---
+++ LES/LES-client/src/java/les/notes/Notes.java
@@ -0,0 +1,17 @@
+package les.notes;
+
+/**
+ *
+ * @author Tyrion
+ */
+public class Notes {
+    
+    /** Glassfish JDBC connection won't ping, server won't be deployed
+     * 
+     * JDBC Connection with Glassfish and MySQL on Netbeans:
+     * http://ushainformatique.com/blog/2010/03/19/jdbcwithglassfish/ 
+     * 
+     *      Can't ping the JDBC on localhost:4848
+     */
+    
+}
