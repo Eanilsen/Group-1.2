6e15c96d24dbd3eb64bb256139a6bf0640cf4749
Jeyloh <jlh.94@hotmail.com>
2015-10-22 22:53:43 +0200

    Added teaching project after tons of work

--- Intro_to_Java_Programming_10th/src/testGUI/TestGUI.java
+++ Intro_to_Java_Programming_10th/src/testGUI/TestGUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testgui;
+package testGUI;
 
 import javafx.geometry.Insets;
 import javafx.application.Application;
