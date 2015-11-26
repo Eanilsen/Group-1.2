b85a62df13d93a359df412398d9f4b916a2966dd
jeyloh <jlh.94@hotmail.com>
2015-11-26 15:27:10 +0100

    added working project

---
+++ SLIT_v02/SLIT_v02-ejb/src/java/entities/Users.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Jons
+ */
+@Entity
+@Table(name = "users")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findByIduser", query = "SELECT u FROM Users u WHERE u.iduser = :iduser"),
+    @NamedQuery(name = "Users.findByFirstname", query = "SELECT u FROM Users u WHERE u.firstname = :firstname"),
+    @NamedQuery(name = "Users.findByLastname", query = "SELECT u FROM Users u WHERE u.lastname = :lastname"),
+    @NamedQuery(name = "Users.findByEmail", query = "SELECT u FROM Users u WHERE u.email = :email"),
+    @NamedQuery(name = "Users.findByLastLogin", query = "SELECT u FROM Users u WHERE u.lastLogin = :lastLogin")})
+public class Users implements Serializable {
+    @JoinTable(name = "available_roles_has_users", joinColumns = {
+        @JoinColumn(name = "users_iduser", referencedColumnName = "iduser")}, inverseJoinColumns = {
+        @JoinColumn(name = "available_roles_idrole", referencedColumnName = "idrole")})
+    @ManyToMany
+    private Collection<AvailableRoles> availableRolesCollection;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "iduser")
+    private Integer iduser;
+    @Size(max = 45)
+    @Column(name = "firstname")
+    private String firstname;
+    @Size(max = 45)
+    @Column(name = "lastname")
+    private String lastname;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 45)
+    @Column(name = "email")
+    private String email;
+    @Column(name = "lastLogin")
+    @Temporal(TemporalType.DATE)
+    private Date lastLogin;
+//    @ManyToMany(mappedBy = "usersCollection")
+//    private Collection<AvailableRoles> availableRolesCollection;
+    @OneToMany(mappedBy = "user")
+    private Collection<Progress> progressCollection;
+    @OneToMany(mappedBy = "createdBy")
+    private Collection<Comment> commentCollection;
+
+    public Users() {
+    }
+
+    public Users(Integer iduser) {
+        this.iduser = iduser;
+    }
+
+    public Integer getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(Integer iduser) {
+        this.iduser = iduser;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    @XmlTransient
+    public Collection<AvailableRoles> getAvailableRolesCollection() {
+        return availableRolesCollection;
+    }
+
+    public void setAvailableRolesCollection(Collection<AvailableRoles> availableRolesCollection) {
+        this.availableRolesCollection = availableRolesCollection;
+    }
+
+    @XmlTransient
+    public Collection<Progress> getProgressCollection() {
+        return progressCollection;
+    }
+
+    public void setProgressCollection(Collection<Progress> progressCollection) {
+        this.progressCollection = progressCollection;
+    }
+
+    @XmlTransient
+    public Collection<Comment> getCommentCollection() {
+        return commentCollection;
+    }
+
+    public void setCommentCollection(Collection<Comment> commentCollection) {
+        this.commentCollection = commentCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (iduser != null ? iduser.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.iduser == null && other.iduser != null) || (this.iduser != null && !this.iduser.equals(other.iduser))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Users[ iduser=" + iduser + " ]";
+    }
+
+//    @XmlTransient
+//    public Collection<AvailableRoles> getAvailableRolesCollection() {
+//        return availableRolesCollection;
+//    }
+//
+//    public void setAvailableRolesCollection(Collection<AvailableRoles> availableRolesCollection) {
+//        this.availableRolesCollection = availableRolesCollection;
+//    }
+    
+}
