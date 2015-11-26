cfd505d4a34b39f30b9734816a15a1573157057b
jeyloh <jlh.94@hotmail.com>
2015-11-26 23:39:15 +0100

    added working project. EJBs are now able to show in GUI!

---
+++ SLIT_v02/SLIT_v02-ejb/build/generated-sources/ap-source-output/entities/Users_.java
@@ -0,0 +1,25 @@
+package entities;
+
+import entities.AvailableRoles;
+import entities.Comment;
+import entities.Progress;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-26T19:09:20")
+@StaticMetamodel(Users.class)
+public class Users_ { 
+
+    public static volatile CollectionAttribute<Users, AvailableRoles> availableRolesCollection;
+    public static volatile SingularAttribute<Users, Date> lastLogin;
+    public static volatile SingularAttribute<Users, Integer> iduser;
+    public static volatile SingularAttribute<Users, String> firstname;
+    public static volatile CollectionAttribute<Users, Comment> commentCollection;
+    public static volatile SingularAttribute<Users, String> email;
+    public static volatile CollectionAttribute<Users, Progress> progressCollection;
+    public static volatile SingularAttribute<Users, String> lastname;
+
+}
---
+++ SLIT_v02/SLIT_v02-ejb/src/java/beans/FileManagerBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Jons
+ */
+@Stateless
+public class FileManagerBean implements FileManagerBeanRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    
+}
