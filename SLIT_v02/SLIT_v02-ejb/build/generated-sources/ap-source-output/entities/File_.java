1e3bb9a7ec02a1a879109b75ca108f22964be633
jeyloh <jlh.94@hotmail.com>
2015-12-01 03:47:33 +0100

    pulled from Master. wrong import in datainitialization fixed

--- SLIT_v02/SLIT_v02-ejb/build/generated-sources/ap-source-output/entities/File_.java
+++ SLIT_v02/SLIT_v02-ejb/build/generated-sources/ap-source-output/entities/File_.java
@@ -6,11 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-01T02:10:10")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-30T22:46:52")
->>>>>>> origin/even
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-01T03:39:57")
 @StaticMetamodel(File.class)
 public class File_ { 
 
