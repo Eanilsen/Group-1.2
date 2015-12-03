9fed0d44f9b961f17b255f477f8bde5dbfc9f5c8
jhinrichs <jonashinrichs@gmx.net>
2015-12-03 01:14:46 +0100

    Pending added

--- SLIT_v02/SLIT_v02-ejb/build/generated-sources/ap-source-output/entities/Ressource_.java
+++ SLIT_v02/SLIT_v02-ejb/build/generated-sources/ap-source-output/entities/Ressource_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-02T14:19:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-02T17:29:46")
 @StaticMetamodel(Ressource.class)
 public class Ressource_ { 
 
